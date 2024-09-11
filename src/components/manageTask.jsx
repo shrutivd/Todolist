9af067b78844747bf19b6c672ea0ca622e5f00f4
shrutivd <svd@pdx.edu>
2024-09-11 16:37:15 -0700

    connected mongodb

--- src/components/manageTask.jsx
+++ src/components/manageTask.jsx
@@ -1,20 +1,62 @@
-import React from 'react'
+import { useState } from "react";
 
 function ManageTask() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Set loading state to true when the request starts
+    try {
+      let result = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      result = await result.json();
+      console.warn(result);
+      if (result) {
+        alert("Data saved successfully");
+        setTitle("");
+        setDescription("");
+      }
+    } catch (error) {
+      console.error("Error saving data:", error);
+      alert("Failed to save data. Please try again.");
+    } finally {
+      setLoading(false); // Reset loading state
+    }
+  };
+
   return (
     <>
-    <div className="flex bg-white">
-      <label for='title'>Title:</label>
-      <input type='text' id='title'/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          id="title"
+          required
+        />
 
-      <label for='description'>Description:</label>
-      <textarea id='description'/> 
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
 
-      <button type='button'>Add Task</button>
-    </div>
+        <button type="submit" disabled={loading || !title || !description}>
+          {loading ? "Saving..." : "Add Task"}
+        </button>
+      </form>
     </>
-
-  )
+  );
 }
 
-export default ManageTask
+export default ManageTask;
